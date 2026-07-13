c372a0a1ef75728d6f8b374d5ed154d0e23717e0
ST33VE D3V 003 <steevenana.dev@gmail.com>
2026-07-13 14:50:24 -0400

    feat: add matrix operations

--- src/bedrocked/math/Matrix4.hpp
+++ src/bedrocked/math/Matrix4.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <array>
+#include <cmath>
+#include <cstddef>
 
 namespace bedrocked {
     class Matrix4 final {
@@ -26,8 +28,49 @@ namespace bedrocked {
             };
         }
 
+        [[nodiscard]] static Matrix4 rotationZ(float angleRadians) noexcept {
+            const float cosine = std::cos(angleRadians);
+            const float sine = std::sin(angleRadians);
+
+            return Matrix4{
+                {
+                    cosine, sine, 0.0F, 0.0F,
+                    -sine, cosine, 0.0F, 0.0F,
+                    0.0F, 0.0F, 1.0F, 0.0F,
+                    0.0F, 0.0F, 0.0F, 1.0F
+                }
+            };
+        }
+
         [[nodiscard]] constexpr const float *data() const noexcept {
             return m_values.data();
+        }
+
+        [[nodiscard]] constexpr Matrix4 operator*(const Matrix4 &other) const noexcept {
+            std::array<float, 16> result{};
+            for (std::size_t column = 0; column < 4; ++column) {
+                for (std::size_t row = 0; row < 4; ++row) {
+                    for (std::size_t index = 0; index < 4; ++index) {
+                        result[column * 4 + row] +=
+                                m_values[index * 4 + row] *
+                                other.m_values[column * 4 + index];
+                    }
+                }
+            }
+            return Matrix4{result};
+        }
+
+        [[nodiscard]] static constexpr Matrix4 scale(
+    float x,
+    float y,
+    float z) noexcept
+        {
+            return Matrix4{{
+                x,    0.0F, 0.0F, 0.0F,
+                0.0F, y,    0.0F, 0.0F,
+                0.0F, 0.0F, z,    0.0F,
+                0.0F, 0.0F, 0.0F, 1.0F
+            }};
         }
 
     private:
