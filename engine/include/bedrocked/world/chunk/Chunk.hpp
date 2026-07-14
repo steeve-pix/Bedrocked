4e7399ff36e2ef0031678b97637cf22480273729
ST33VE D3V 003 <steevenana.dev@gmail.com>
2026-07-14 19:48:19 -0400

    feat: add chunk block storage

--- engine/include/bedrocked/world/chunk/Chunk.hpp
+++ engine/include/bedrocked/world/chunk/Chunk.hpp
@@ -3,7 +3,6 @@
 #include <array>
 #include <cstddef>
 
-
 namespace bedrocked {
     class Chunk final {
     public:
@@ -17,10 +16,10 @@ namespace bedrocked {
         void setBlock(std::size_t x, std::size_t y, std::size_t z,BlockType type) noexcept;
 
     private:
-        std::array<BlockType, BlockCount> m_blocks{};
-
         [[nodiscard]] static constexpr std::size_t index(std::size_t x, std::size_t y, std::size_t z) noexcept {
             return x + Width * (z + Depth * y);
         }
+
+        std::array<BlockType, BlockCount> m_blocks{};
     };
 }
