ae2d206ee4510e14c4ee1ae7e66b4074b287fe12
ST33VE D3V 003 <steevenana.dev@gmail.com>
2026-07-14 19:57:31 -0400

    refactor: add safe chunk coordinate handling

--- engine/include/bedrocked/world/chunk/Chunk.hpp
+++ engine/include/bedrocked/world/chunk/Chunk.hpp
@@ -13,11 +13,20 @@ namespace bedrocked {
 
         [[nodiscard]] BlockType block(std::size_t x, std::size_t y, std::size_t z) const noexcept;
 
-        void setBlock(std::size_t x, std::size_t y, std::size_t z,BlockType type) noexcept;
+        void setBlock(int x, int y, int z, BlockType type) noexcept;
 
     private:
-        [[nodiscard]] static constexpr std::size_t index(std::size_t x, std::size_t y, std::size_t z) noexcept {
+        [[nodiscard]] static constexpr std::size_t index(int x, int y, int z) noexcept {
             return x + Width * (z + Depth * y);
+        }
+
+        [[nodiscard]] static constexpr bool contains(int x, int y, int z) noexcept {
+            return x >= 0 &&
+                   y >= 0 &&
+                   z >= 0 &&
+                   x < static_cast<int>(Width) &&
+                   y < static_cast<int>(Height) &&
+                   z < static_cast<int>(Depth);
         }
 
         std::array<BlockType, BlockCount> m_blocks{};
